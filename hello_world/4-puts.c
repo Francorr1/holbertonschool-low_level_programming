5aaf50bdf7deb7cdc65dd571f88b60ac675c2933
Franco <francorr240@gmail.com>
2023-02-15 06:13:35 -0800

    Commit automated by gitpush

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-  char txt = "Programming is like building a multilingual puzzle\n";
-  puts(txt);
+  puts("Programming is like building a multilingual puzzle");
   return 0;
 }

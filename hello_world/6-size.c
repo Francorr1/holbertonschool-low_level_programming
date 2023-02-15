3ef4abd59fba2cfac4b00822f3856c52de081ba4
Franco <francorr240@gmail.com>
2023-02-15 09:03:12 -0800

    Commit automated by gitpush

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Size of
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("Size of a char: %lu\n", sizeof(char));
+printf("Size of an int: %lu\n", sizeof(int));
+printf("Size of a long int: %lu\n", sizeof(long int));
+printf("Size of a long long int: %lu\n", sizeof(long long int));
+printf("Size of a float: %lu\n", sizeof(float));
+return (0);
+}

b22b5775e7970ff4a6c57711c54ac39164093074
Franco <francorr240@gmail.com>
2023-02-16 05:09:58 -0800

    Commit automated by gitpush

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -11,10 +11,10 @@
 int main(void)
 {
 int n;
-
+int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int last_digit = n % 10;
+last_digit = n % 10;
 if (last_digit > 5)
 {
 printf("Last digit of %i is %i and is greater than 5\n", n, last_digit);
@@ -23,7 +23,7 @@ if (last_digit == 0)
 {
 printf("Last digit of %i is %i and is 0\n", n, last_digit);
 }
-if (last_digit < 6 != 0)
+if (last_digit < 6 && last_digit != 0)
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_digit);
 }

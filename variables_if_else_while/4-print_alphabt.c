2b8ce9e8976c264f24cfb9fd7218aa643982797f
Franco <francorr240@gmail.com>
2023-02-16 10:55:23 -0800

    Commit automated by gitpush

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Size of
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+{
+if (c == 'q' || c == 'e')
+++c;
+else
+putchar(c);
+}
+putchar('\n');
+return (0);
+}

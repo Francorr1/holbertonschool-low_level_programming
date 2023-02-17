1822d7c20364049596b33321b59deaf8e6022c87
Franco <francorr240@gmail.com>
2023-02-17 09:21:13 -0800

    Commit automated by gitpush

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - Entry point
+ * print_alphabet_x10 - Prints the alphabet 10 times
  *
  * Description: Size of
  *
@@ -10,7 +10,8 @@ void print_alphabet_x10(void)
 {
 char i;
 int t;
-for (t = 0; t <= 9; t++){
+for (t = 0; t <= 9; t++)
+{
 for (i = 'a'; i <= 'z'; i++)
 _putchar(i);
 _putchar('\n');

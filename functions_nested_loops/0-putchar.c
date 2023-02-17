7cdffb9f1be1699df58d69d0ee16982e457eed5e
Franco <francorr240@gmail.com>
2023-02-17 07:35:57 -0800

    Commit automated by gitpush

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Size of
+ *
+ * Return: On success 0
+ */
+int main(void)
+{
+char frase[] = "_putchar";
+int i;
+for (i = 0; i <= '7'; ++i)
+_putchar(frase[i]);
+return (0);
+}

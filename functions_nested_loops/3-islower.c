ba4703604938b5bc5cea26d43ff08a568bc0637f
Franco <francorr240@gmail.com>
2023-02-17 09:51:36 -0800

    Commit automated by gitpush

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - Entry point
+ *
+ *
+ * Description: Size of
+ *
+ * Return: On success 0
+ */
+int _islower(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (0);
+else if (c >= 'a' && c <= 'z')
+return (1);
+}

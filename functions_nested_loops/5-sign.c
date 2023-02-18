dbcae08d08dd8237bd9201e57fd9a273ba2463ac
Franco <francorr240@gmail.com>
2023-02-18 09:22:28 -0800

    Commit automated by gitpush

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 int r;
 if (n == 0)
 {
-r = 0
+r = 0;
 _putchar('0');
 }
 if (n >= 0)
 {
-r = 1
+r = 1;
 _putchar('+');
 }
 if (n <= 0)
 {
-r = -1
+r = -1;
 _putchar('-');
 }
 return (r);

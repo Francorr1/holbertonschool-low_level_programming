9933279dc66210c8a13c4b2488cd1e35579e61fb
Franco <francorr240@gmail.com>
2023-02-18 09:21:18 -0800

    Commit automated by gitpush

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: char
+ * Description: Size of
+ *
+ * Return: On success 0
+ */
+int print_sign(int n)
+{
+int r;
+if (n == 0)
+{
+r = 0
+_putchar('0');
+}
+if (n >= 0)
+{
+r = 1
+_putchar('+');
+}
+if (n <= 0)
+{
+r = -1
+_putchar('-');
+}
+return (r);
+}

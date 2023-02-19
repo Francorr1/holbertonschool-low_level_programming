c478d9bdf6cfcb7bd64d7fee852d11667a3fdf1b
Franco <francorr240@gmail.com>
2023-02-19 11:33:26 -0800

    Commit automated by gitpush

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,16 @@ int print_last_digit(int n)
 {
 	int i;
 
-	i = n % 10;
-	_putchar((n % 10) + '0');
+	if (n > 0)
+	{
+		i = n % 10;
+		_putchar(i + '0');
+	}
+	if (n < 0)
+	{
+		n = n * -1;
+		i = n % 10;
+                _putchar(i + '0');
+	}
 	return (i);
 }

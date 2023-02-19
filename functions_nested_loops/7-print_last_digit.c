0c0566bb8fe160b728825c08c05840ac85d552b9
Franco <francorr240@gmail.com>
2023-02-19 10:33:08 -0800

    Commit automated by gitpush

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @n: char
+ * Description: Size of
+ *
+ * Return: On success 0
+ */
+int print_last_digit(int n)
+{
+	int i;
+
+	i = n % 10;
+	return (i);
+}

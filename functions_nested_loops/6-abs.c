564616d04ded2b52e7e9cb7f4627021a82cbe82c
Franco <francorr240@gmail.com>
2023-02-19 06:38:33 -0800

    Commit automated by gitpush

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -9,13 +9,16 @@
 int _abs(int n)
 {
 	int i;
+	int l;
 
+	l = '0';
 	i = '0';
 	if (n > '0')
 	{
 		while (i != n)
 		{
 			i++;
+			l++;
 		}
 	}
 	if (n < '0')
@@ -23,7 +26,8 @@ int _abs(int n)
 		while (i != n)
 		{
 			i--;
+			l++;
 		}
 	}
-	return (i);
+	return (l);
 }

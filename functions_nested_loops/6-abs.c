048b7c55da6340d7cba1b1a165d065afd282772e
Franco <francorr240@gmail.com>
2023-02-19 06:35:17 -0800

    Commit automated by gitpush

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ * @n: char
+ * Description: Size of
+ *
+ * Return: On success 0
+ */
+int _abs(int n)
+{
+	int i;
+
+	i = '0';
+	if (n > '0')
+	{
+		while (i != n)
+		{
+			i++;
+		}
+	}
+	if (n < '0')
+	{
+		while (i != n)
+		{
+			i--;
+		}
+	}
+	return (i);
+}

f3f1d1208ca2c37fe2bb8bc94d3821eb9e1e4326
Franco <francorr240@gmail.com>
2023-02-22 11:41:40 -0800

    Commit automated by gitpush

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ * @n: int
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar(95);
+	}
+	else
+		_putchar('\n');
+	_putchar('\n');
+}

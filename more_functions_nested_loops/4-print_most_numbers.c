5b188b824fe6e340561ab2f6ae90296500722728
Franco <francorr240@gmail.com>
2023-02-22 10:38:30 -0800

    Commit automated by gitpush

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entry point
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == 2 || i == 4)
+			i++;
+		_putchar(i);
+	}
+}

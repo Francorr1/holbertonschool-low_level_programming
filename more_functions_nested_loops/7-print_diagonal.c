e694eba839b77ace431fad61f118eb340a2cd264
Franco <francorr240@gmail.com>
2023-02-24 04:01:38 -0800

    Commit automated by gitpush

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * print_line - Entry point
+ * print_diagonal - Entry point
  * @n: int
  */
 void print_diagonal(int n)
 {
-        int i;
+	int i;
 	int j;
 
-        if (n > 0)
-        {
-                for (i = 0; i < n; i++)
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
 				_putchar(32);
 			_putchar(92);
 			_putchar('\n');
 		}
-        }
+	}
 	if (n <= 0)
-        	_putchar('\n');
+		_putchar('\n');
 }

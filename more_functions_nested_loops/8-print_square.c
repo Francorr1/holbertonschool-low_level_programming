9ea441df1631ba41a6bfe367b5b0d3cc3ac5ddd3
Franco <francorr240@gmail.com>
2023-02-24 04:07:09 -0800

    Commit automated by gitpush

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - Entry point
+ * @n: int
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar(35);
+			_putchar('\n');
+		}
+	}
+	if (n <= 0)
+		_putchar('\n');
+}

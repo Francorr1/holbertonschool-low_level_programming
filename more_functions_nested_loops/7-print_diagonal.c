7d92a353db29fa98724df2e0077f4df58cafa928
Franco <francorr240@gmail.com>
2023-02-24 03:58:38 -0800

    Commit automated by gitpush

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -15,7 +15,9 @@ void print_diagonal(int n)
 			for (j = 0; j < i; j++)
 				_putchar(32);
 			_putchar(92);
+			_putchar('\n');
 		}
         }
-        _putchar('\n');
+	if (n <= 0)
+        	_putchar('\n');
 }

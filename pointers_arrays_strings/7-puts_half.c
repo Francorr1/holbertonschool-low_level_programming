4608b51cd7ed80541cac695db1d0b0ccd55a68a3
Franco <francorr240@gmail.com>
2023-02-28 07:39:35 -0800

    Commit automated by gitpush

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *
- *
+ * puts_half - Main Function
+ * @str: Address of str
  */
 void puts_half(char *str)
 {
@@ -11,6 +11,16 @@ void puts_half(char *str)
 	for (i = 0; str[i]; i++)
 		continue;
 	n = i / 2;
-	for (; n <= i; n++)
-		_putchar(str[n]);
+	if (n % 2 == 0)
+	{
+		for (; n <= i; n++)
+			_putchar(str[n]);
+	}
+	else
+	{
+		n++;
+		for (; n <= i; n++)
+			_putchar(str[n]);
+	}
+	_putchar(10);
 }

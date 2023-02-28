643fa1a10affe884f6abaeddfcad2f7178e204b0
Franco <francorr240@gmail.com>
2023-02-28 15:12:34 -0800

    Commit automated by gitpush

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Main function
+ * @a: Address of a
+ * @n: An int
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i == n)
+		{
+			printf("%d", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+	_putchar('\n');
+}

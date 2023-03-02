45e7bf0277e1e90fa84d65c678238eb00df47862
Franco <francorr240@gmail.com>
2023-03-01 20:59:04 -0800

    Commit automated by gitpush

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,30 @@
 #include "main.h"
 /**
+ * _strncat - Main Function
+ * @dest: Address of dest
+ * @src: Address of src
+ * @n: An int
  *
- *
- *
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
-        int i;
-        int j;
-        int ij;
-        int d;
+	int i;
+	int j;
+	int ij;
+	int d;
 
-        d = 0;
-        ij = 0;
-        for (i = 0; src[i]; i++)
-                continue;
-        for (j = 0; dest[j]; j++)
-                continue;
-        ij = i + n;
-        for (; j <= ij; j++)
-        {
-                dest[j] = src[d];
-                d++;
-        }
-        return (dest);
+	d = 0;
+	ij = 0;
+	for (i = 0; src[i]; i++)
+		continue;
+	for (j = 0; dest[j]; j++)
+		continue;
+	ij = i + n;
+	for (; j <= ij; j++)
+	{
+		dest[j] = src[d];
+		d++;
+	}
+	return (dest);
 }

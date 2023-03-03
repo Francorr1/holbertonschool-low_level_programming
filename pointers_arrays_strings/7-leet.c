33ac6999d8de44f66c77ed832644c2f8e360694a
Franco <francorr240@gmail.com>
2023-03-03 09:52:25 -0800

    Commit automated by gitpush

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
+ * leet - Main Function
+ * @s: Address of s
  *
- *
+ * Return: The new string
  */
 char *leet(char *s)
 {
-	char og[10] = {97, 101, 111, 116, 108, 65, 69, 79, 84, 76};
-	char rep[10] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+	char og[10] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+	int rep[10] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
 	int i;
 	int n;
 
@@ -15,7 +17,9 @@ char *leet(char *s)
 		for (n = 0; og[n]; n++)
 		{
 			if (s[i] == og[n])
+			{
 				s[i] = rep[n];
+			}
 		}
 	}
 	return (s);

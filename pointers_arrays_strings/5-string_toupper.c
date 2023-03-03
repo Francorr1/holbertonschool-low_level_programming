ccb9e86c49f1b14f34e49c66b0633a708ed07b48
Franco <francorr240@gmail.com>
2023-03-03 05:07:18 -0800

    Commit automated by gitpush

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -5,19 +5,20 @@
  *
  * Return: res
  */
+char res[1000];
+
 char *string_toupper(char *s)
 {
 	int i;
-	char res;
 
 	for (i = 0; s[i]; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			res = s[i] - 32;
+			res[i] = s[i] - 32;
 		}
 		else
 			continue;
 	}
-	return (res);
+	return (*res);
 }

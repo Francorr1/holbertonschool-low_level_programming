338f3410061c37900a660335f7c8b3986c737e27
Franco <francorr240@gmail.com>
2023-03-03 06:47:14 -0800

    Commit automated by gitpush

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,15 +6,15 @@
  */
 char *cap_string(char *s)
 {
-	char[12] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	char cap[12] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 	int i;
 	int n;
 
 	for (i = 0; s[i]; i++)
 	{
-		for (n = 0; char[n]; n++)
+		for (n = 0; cap[n]; n++)
 		{
-			if (s[i] == char[n])
+			if (s[i] == cap[n])
 			{
 				i++;
 				s[i] -= 32;

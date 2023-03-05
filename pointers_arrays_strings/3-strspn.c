53e4cff54751acac7922b0b5102733964664cd83
Franco <francorr240@gmail.com>
2023-03-05 11:25:55 -0800

    Commit automated by gitpush

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int n;
+	int c;
+
+	c = 0;
+	for(i = 0; s[i]; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (n = 0; accept[n]; n++)
+			{
+				if (s[i] == accept[n])
+				{
+					n++;
+				}
+			}
+		}
+		else
+		{
+			return (c);
+		}
+	}
+	return(c);
+}

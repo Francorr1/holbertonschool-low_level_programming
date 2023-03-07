d14a867b2313374712f21fcbc26e62bddd5aaa53
Franco <francorr240@gmail.com>
2023-03-07 05:32:19 -0800

    Commit automated by gitpush

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - Main function
+ * @x: an int
+ * @y: an int
+ *
+ * Return: something lmao
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (x);
+	}
+	if (y < 0)
+	{
+		return (-1);
+	}
+	x = x * y;
+	y--;
+	_pow_recursion(x, y);
+	return (x);
+}

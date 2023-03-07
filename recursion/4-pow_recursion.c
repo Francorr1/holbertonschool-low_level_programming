0f7a721f8df245f03822aaf3834e1d143673e065
Franco <francorr240@gmail.com>
2023-03-07 10:11:28 -0800

    Commit automated by gitpush

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,16 +8,17 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int i;
+
+
 	if (y == 0)
 	{
-		return (x);
+		return (1);
 	}
 	if (y < 0)
 	{
 		return (-1);
 	}
-	x = x * x;
-	y--;
-	_pow_recursion(x, y);
-	return (x);
+	i = x* _pow_recursion(x, y - 1);
+	return (res);
 }

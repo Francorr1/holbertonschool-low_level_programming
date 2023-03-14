39cc508dbbd12b861fdcf90a01e9eecb603bd004
Franco <francorr240@gmail.com>
2023-03-13 17:44:08 -0700

    Commit automated by gitpush

---
+++ malloc_free/0-create-array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * create_array - Main function
+ * @c: A pointer to c
+ * @size: The size lmao
+ *
+ * Return: The result
+ */
+char *create_array(unsigned int size, char c)
+{
+	c = malloc(size);
+	int i;
+
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; c[i]; i++)
+	{
+		continue;
+	}
+	if (i == 0)
+	{
+		return (NULL);
+	}
+	return (c);
+}

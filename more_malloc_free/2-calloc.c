bfe94e153bd715f8ce28ab9e96e31b8ed7ba66e1
Franco <francorr240@gmail.com>
2023-03-17 03:22:12 -0700

    Commit automated by gitpush

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - Main function
+ * @nmemb: An int
+ * @size: another int
+ *
+ * Return: result
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i;
+	int *p = malloc(size * nmemb);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
+	return (p);
+}

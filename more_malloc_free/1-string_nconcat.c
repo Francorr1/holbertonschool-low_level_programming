555a6f12326d9b90e6c69cd16a34e209b1c7ba29
Franco <francorr240@gmail.com>
2023-03-18 21:17:04 -0700

    Commit automated by gitpush

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - Main function
+ * @s1: First string
+ * @s2: Second string
+ * @n: An int
+ *
+ * Return: The result
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len1, len2;
+	char *p;
+
+	for (len1 = 0; s1[len1]; len1++)
+		continue;
+	for (len2 = 0; s2[len2]; len2++)
+		continue;
+	if (len2 >= n)
+		n = len2;
+	p = malloc(sizeof(char) * (len1 + n));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		n[1] = s1[i];
+	for (j = 0; j <= n; j++)
+	{
+		p[i] = s2[j];
+	}
+	return (p);
+}

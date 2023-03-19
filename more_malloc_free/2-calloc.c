df8c511cb727dc32ad80b6dea8b3f9d4c3e14005
Franco <francorr240@gmail.com>
2023-03-18 20:41:51 -0700

    Commit automated by gitpush

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	unsigned int *p = malloc(nmemb * size);
+	unsigned int *p = malloc(size * nmemb);
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);

a3fab38e2fbe48c65b302d2a30f0565dc262c14e
Franco <francorr240@gmail.com>
2023-03-20 06:28:16 -0700

    Commit automated by gitpush

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef structureofadog
+#define structureofadog
+/**
+ * struct dog - Structure of a dog
+ * @name: It's name
+ * @age: It's age
+ * @owner: It's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

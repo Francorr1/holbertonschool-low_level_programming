3579c689c4160616c4095b2953ee1cbcf34ce122
Franco <francorr240@gmail.com>
2023-03-20 09:12:43 -0700

    Commit automated by gitpush

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - Inits the dog structure
+ * @d: pointer to d
+ * @name: It's name
+ * @age: It's age
+ * @owner: It's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

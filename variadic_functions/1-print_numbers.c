2e64399f6ec08699dfe140014b4c5305a2938215
Franco <francorr240@gmail.com>
2023-03-26 10:36:31 -0700

    Commit automated by gitpush

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - Prints all numbers given to it
+ * @separator: Used to divide the printed numbers
+ * @n: Numbers to print
+ */
+void print_numbers(const char *separator, const unsigned int n, ...);
+{
+	unsigned int i;
+	va_list val;
+
+	va_start(val, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(val, int));
+		if (separator != NULL && i != n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(val);
+}

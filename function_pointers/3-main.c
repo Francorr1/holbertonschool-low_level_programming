a14ed7025220022d6c63b10998abbca5922182f9
Franco <francorr240@gmail.com>
2023-03-26 09:18:45 -0700

    Commit automated by gitpush

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	op = argv[2];
-	if (get_op_func(argv[2]) == NULL)
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);

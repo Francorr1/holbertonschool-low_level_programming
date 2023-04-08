799f51a577cab413532f98672900e0a09a15de2d
Franco <francorr240@gmail.com>
2023-04-08 10:51:56 -0700

    task 4 of doubly linked lists

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_dlistint - Frees a linked list
+ * @head: The list to be freed
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t temp;
+
+	if (head)
+	{
+		while (head)
+		{
+			temp = head;
+			head = head->next;
+			free(temp->n);
+			free(temp);
+		}
+	}
+}

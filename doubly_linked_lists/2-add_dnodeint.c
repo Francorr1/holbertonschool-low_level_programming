b3abccd4f4fd645a5b18e3f74a67fc3e5f135a60
Franco <francorr240@gmail.com>
2023-04-08 10:34:23 -0700

    Task 2 of Doubly liked lists

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
-	if (n == '\0')
+	if (head == NULL)
 	{
 		return (NULL);
 	}
@@ -20,6 +20,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new->n = n;
 		new->prev = NULL;
 		new->next = *head;
+		if (*head)
+		{
+			(*head)->prev = new;
+		}
 		*head = new;
 	}
 	else

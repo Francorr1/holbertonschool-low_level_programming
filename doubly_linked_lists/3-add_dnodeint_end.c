b1e6882543e3a5ed49528308ee19e32d0e461657
Franco <francorr240@gmail.com>
2023-04-08 10:41:59 -0700

    Task 2 of Doubly liked lists

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - Adds a new node at the end of a list
+ * @head: Said list
+ * @n: The value to store in the new node
+ *
+ * Return: The address of the new node
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	while ((*head) =! NULL)
+	{
+		(*head) = (*head)->next;
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (new)
+	{
+		new->n = n;
+		new->prev = *head;
+		new->next = NULL;
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (new);
+}

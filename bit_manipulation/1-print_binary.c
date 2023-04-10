17d986ca7b20d17af1f8f3ada709b16c1ea54693
Franco <francorr240@gmail.com>
2023-04-10 09:02:33 -0700

    Task 1 of Bit manipulation

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -10,12 +10,12 @@ void print_binary(unsigned long int n)
 	while (n != 0)
 	{
 		bin = n & 1;
-		n = n >> 1;
+		n = n << 1;
 		if (bin == 1)
 		{
 			_putchar('1');
 		}
-		if (bin == 0)
+		if (bin ==0)
 		{
 			_putchar('0');
 		}

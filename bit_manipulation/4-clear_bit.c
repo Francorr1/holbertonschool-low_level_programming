0f8bed305618c96a960c7a2d0e865d10ccd50771
Franco <francorr240@gmail.com>
2023-04-10 10:14:21 -0700

    Task 4 of Bit manipulation

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * clear_bit - Takes the bit in the position INDEX and makes it a 0
+ * @n: Where to change the bit
+ * @index The bit to change
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+	*n &= ~(1 << index);
+	return (1);
+}

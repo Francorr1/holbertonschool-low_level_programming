a9f0a3c439db118e9e6aadd6341fe3038aade7c3
Franco <francorr240@gmail.com>
2023-04-10 08:21:55 -0700

    Task 1 of Bit manipulation

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_binary - Converts a decimal number to binary
+ * @n: Said decimal number
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int bin = 0;
+
+	while (n != 0)
+	{
+		bin = n & 1;
+		n = n >> 1;
+		_putchar(bin);
+	}
+}

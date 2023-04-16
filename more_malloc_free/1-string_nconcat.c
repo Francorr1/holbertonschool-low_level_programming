6ad14037df05b26bde10475dfbe429331d6d5760
Franco <francorr240@gmail.com>
2023-04-16 16:05:20 -0700

    Commit automated by gitpush

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		p[i] = s1[i];
-	for (j = 0; j <= n; j++)
+	for (j = 0; j < n; j++)
 	{
 		p[i] = s2[j];
 		i++;

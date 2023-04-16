846e8662c982ca0979b55755a9c88e71e6f37563
Franco <francorr240@gmail.com>
2023-04-16 16:14:15 -0700

    Task 3 of Doubly linked lists

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,18 +9,19 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+	dlistint_t *h = (*head);
 
-	while ((*head)->next != NULL)
+	while (h->next != NULL)
 	{
-		(*head) = (*head)->next;
+		h = h->next;
 	}
 	new = malloc(sizeof(dlistint_t));
 	if (new)
 	{
 		new->n = n;
-		new->prev = *head;
+		new->prev = h;
 		new->next = NULL;
-		(*head)->next = new;
+		h->next = new;
 	}
 	else
 	{

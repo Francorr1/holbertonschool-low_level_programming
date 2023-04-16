27f5448a3be95e4c76dc112aaac7dd6e88b844c8
Franco <francorr240@gmail.com>
2023-04-16 01:07:08 -0700

    Task 3 of File I/O

---
+++ file_io/3-cp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *
+ */
+int main(int argc, char *argv[])
+{
+	int origin, dest, conto, contd, close;
+	char *buff[1024];
+
+	if (argc != 2)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	origin = open(argv[1], O_RDONLY);
+	if ((conto = read(origin, buff, sizeof(buff))) != 0)
+	{
+		contd = write(des, buff, conto);
+	}
+	if (origin == -1 || conto == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (dest == -1 || contd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	close = close(origin);
+	if (close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close %d\n", origin);
+		exit(100);
+	}
+	return(0);
+}

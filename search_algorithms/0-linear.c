a6117e0a3f0d4d2fb2ca22629c25348194ec9214
Franco <francorr240@gmail.com>
2023-05-25 13:49:43 -0700

    Task 0 completed

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -13,7 +13,7 @@ int linear_search(int *array, size_t size, int value)
     }
     for (; i < size; i++)
     {
-        printf("Value checked array[%i] = [%i]", i, array[i]);
+        printf("Value checked array[%li] = [%i]", i, array[i]);
         if (array[i] == value)
         {
             return (i);

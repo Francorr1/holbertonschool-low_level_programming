fbc9f59709a214e073b5e5babb4757b056afdc4a
Franco <francorr240@gmail.com>
2023-05-25 13:44:48 -0700

    Added header file and completed task 0

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef PROTEKTOR
+#define PROTEKTOR
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/* Function prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif

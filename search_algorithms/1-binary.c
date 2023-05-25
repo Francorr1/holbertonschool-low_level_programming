9ff1037448caad58dcb9b733c5d8aa111b9c967a
Franco <francorr240@gmail.com>
2023-05-25 15:14:39 -0700

    Task 1 completed

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,39 +1,45 @@
 #include "search_algos.h"
 /**
- * 
- * 
+ * binary_search - Looks for a value using binary search
+ * @array: The array to search
+ * @size: Size of said array
+ * @value: The value to look for
+ *
+ * Return: The index of the value or -1 if it's not there
 */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t l = 0, h = 0, m = 0;
+	size_t l = 0, h = 0, m = 0;
 
-    if (!array)
-    {
-        return (-1);
-    }
-    h = size - 1;
-    while (l <= h)
-    {
-        m = (l + h) / 2;
-        print_array(array, l, h);
-        if (array[m] == value)
-        {
-            return (m);
-        }
-        else if (value > array[m])
-        {
-            l = m + 1;
-        }
-        else
-        {
-            h = m - 1;
-        }
-    }
-    return (-1);
+	if (!array)
+	{
+		return (-1);
+	}
+	h = size - 1;
+	while (l <= h)
+	{
+		m = (l + h) / 2;
+		print_array(array, l, h);
+		if (array[m] == value)
+		{
+			return (m);
+		}
+		else if (value > array[m])
+		{
+			l = m + 1;
+		}
+		else
+		{
+			h = m - 1;
+		}
+	}
+	return (-1);
 }
 /**
- * 
- * 
+ * print_array - Prints a portion of an array
+ * @array: The array to print
+ * @l: The lowest index to print
+ * @h: The highest index to print
 */
 void print_array(int *array, size_t l, size_t h)
 {

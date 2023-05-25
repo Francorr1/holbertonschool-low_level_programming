047d7ed9e36177cb4822a3e8358a8e8f89269d16
Franco <francorr240@gmail.com>
2023-05-25 15:09:49 -0700

    Task 1 completed

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -9,5 +9,6 @@
 /* Function prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t l, size_t h);
 
 #endif
